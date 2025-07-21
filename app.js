70370eb402e6f1b0d6f11613f7c860d792bd76ee
Vaishnavi Singh <vaishnavisingh.rkt@gmail.com>
2025-07-21 13:06:56 +0530

    added files

---
+++ app.js
@@ -0,0 +1,59 @@
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg= document.querySelector(".msg");
+
+document.addEventListener("Load", () =>{
+
+})
+
+let i=0;
+for (let select of dropdowns){
+    for (currCode in countryList){
+        let newoption = document.createElement("option");
+        newoption.innerText = currCode;
+        newoption.value = currCode;
+        if(select.name === "from" && currCode === "USD"){
+            newoption.selected = "selected";
+        }else if(select.name === "to" && currCode === "INR"){
+            newoption.selected = "selected";
+        }
+        select.append(newoption);
+    }
+
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+
+const updateFlag = (element) => {
+   let currCode = element.value;
+   let countryCode = countryList[currCode];
+   let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+   let img = element.parentElement.querySelector("img");
+   img.src = newSrc;
+}
+
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+
+let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if(amtVal === "" || amtVal < 1){
+        amtVal = 1;
+        amount.value = "1";
+    }
+    
+    const URL= `https://api.exchangerate-api.com/v4/latest/${fromCurr.value}`;
+    let response =  await fetch(URL);
+    let data = await response.json();
+    let rate = data.rates[toCurr.value];
+    console.log(rate);
+    let finalAmount = amtVal*rate;
+    msg.innerText = `${amtVal}  ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+});
+
+
+
